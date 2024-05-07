053f01babb495054cd2857df8f2131028b17af32
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2024-05-07 15:07:38 +0200

    Version bump, support for Flan 1.10+

--- src/main/java/me/emafire003/dev/lightwithin/compat/flan/FlanCompat.java
+++ src/main/java/me/emafire003/dev/lightwithin/compat/flan/FlanCompat.java
@@ -1,27 +1,23 @@
 package me.emafire003.dev.lightwithin.compat.flan;
 
+import io.github.flemmli97.flan.Flan;
 import io.github.flemmli97.flan.api.ClaimHandler;
-import io.github.flemmli97.flan.api.permission.ClaimPermission;
-import io.github.flemmli97.flan.api.permission.PermissionRegistry;
 import me.emafire003.dev.lightwithin.config.Config;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 
 public class FlanCompat {
 
-    public static ClaimPermission ACTIVATE_LIGHT = PermissionRegistry.register(new ClaimPermission("ACTIVATE_LIGHT", () -> new ItemStack(Items.LIGHT),true,
-            "Permission to activate an InnerLight"));
-    public static ClaimPermission LIGHT_GRIEFING = PermissionRegistry.register(new ClaimPermission("LIGHT_GRIEFING", () -> new ItemStack(Items.REDSTONE_LAMP),true,
-            "Permission to modify the terrain during a light activation"));
+    public static Identifier ACTIVATE_LIGHT = new Identifier(Flan.MODID, "activate_innerlight");
+    public static Identifier LIGHT_GRIEFING = new Identifier(Flan.MODID, "innerlight_griefing");
+
 
     public static boolean canActivateHere(ServerPlayerEntity player, BlockPos pos){
         try{
             //It works unless you own that claim etc
-            boolean can = ClaimHandler.canInteract(player, pos, ACTIVATE_LIGHT);
-
-            return can;
+            return ClaimHandler.canInteract(player, pos, ACTIVATE_LIGHT);
         }catch (NullPointerException e){
             //It means there is no chunk
             return Config.LIGHT_DEFAULT_STATUS;
@@ -29,7 +25,16 @@ public class FlanCompat {
     }
 
     public static boolean canActivateHereGriefing(ServerPlayerEntity player, BlockPos pos){
-        return ClaimHandler.canInteract(player, pos, LIGHT_GRIEFING);
+        try{
+            player.sendMessage(Text.literal("Yes the problem is flan. Checking GRIEFING LIGHT: " + ClaimHandler.canInteract(player, pos, LIGHT_GRIEFING)));
+
+            //It works unless you own that claim etc
+
+            return ClaimHandler.canInteract(player, pos, LIGHT_GRIEFING);
+        }catch (NullPointerException e){
+            //It means there is no chunk
+            return Config.LIGHT_DEFAULT_STATUS;
+        }
     }
 
     public static void registerFlan(){
