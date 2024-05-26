8460a3e317320467b3130f35799898deecda875b
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2024-05-26 15:20:32 +0200

    Added support for old BottledLight that use nbt

--- src/main/java/me/emafire003/dev/lightwithin/items/BottledLightItem.java
+++ src/main/java/me/emafire003/dev/lightwithin/items/BottledLightItem.java
@@ -4,6 +4,7 @@ import me.emafire003.dev.lightwithin.LightWithin;
 import me.emafire003.dev.lightwithin.component.LightComponent;
 import me.emafire003.dev.lightwithin.config.Config;
 import me.emafire003.dev.lightwithin.items.components.LightItemComponents;
+import me.emafire003.dev.lightwithin.items.crafting.BrewRecipes;
 import me.emafire003.dev.lightwithin.lights.InnerLightType;
 import me.emafire003.dev.lightwithin.particles.LightParticlesUtil;
 import me.emafire003.dev.lightwithin.sounds.LightSounds;
@@ -12,6 +13,8 @@ import me.emafire003.dev.lightwithin.util.TargetType;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipType;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
@@ -64,7 +67,6 @@ public class BottledLightItem extends Item {
             //NbtCompound nbt = stack.getNbt();
 
             if(stack.getComponents().contains(LightItemComponents.BOTTLED_LIGHT_TYPE_INGREDIENT)){
-
                 InnerLightType bottled_type = InnerLightType.valueOf(stack.getComponents().get(LightItemComponents.BOTTLED_LIGHT_TYPE_INGREDIENT));
                 if(component.getType().equals(bottled_type)){
                     if(stack.getComponents().contains(LightItemComponents.BOTTLED_LIGHT_TARGET_INGREDIENT)){
@@ -99,6 +101,49 @@ public class BottledLightItem extends Item {
                     world.createExplosion(item, user.getX(), user.getY()+1, user.getZ(), 1.5f, false, World.ExplosionSourceType.MOB);
                     return TypedActionResult.consume(stack);
                 }
+                //Checks if the item is created in older versions, so it may have NBT data instead of custom data
+            }else if(stack.contains(DataComponentTypes.CUSTOM_DATA)) {
+                NbtComponent nbtComponent = stack.get(DataComponentTypes.CUSTOM_DATA);
+                if(nbtComponent == null){
+                    return TypedActionResult.pass(stack);
+                }
+                if(nbtComponent.contains(BrewRecipes.TYPE_INGREDIENT_KEY)){
+                    InnerLightType bottled_type = InnerLightType.valueOf(nbtComponent.copyNbt().getString(BrewRecipes.TYPE_INGREDIENT_KEY));
+                    if(component.getType().equals(bottled_type)){
+                        if(nbtComponent.contains(BrewRecipes.TARGET_INGREDIENT_KEY)){
+                            TargetType bottled_target = TargetType.valueOf(nbtComponent.copyNbt().getString(BrewRecipes.TARGET_INGREDIENT_KEY));
+                            if(component.getTargets().equals(bottled_target)){
+                                if(addCharge(user, stack, component)){
+                                    return TypedActionResult.consume(stack);
+                                }else{
+                                    return TypedActionResult.pass(stack);
+                                }
+                            }else{
+                                user.getWorld().addBlockBreakParticles(user.getBlockPos().up(), Blocks.GLASS.getDefaultState());
+                                user.playSound(SoundEvents.BLOCK_GLASS_BREAK, 0.43f, 1.3f);
+                                user.playSound(SoundEvents.BLOCK_FIRE_EXTINGUISH, 0.6f, 1.7f);
+                                stack.decrement(1);
+                                return TypedActionResult.consume(stack);
+                            }
+                        }else{
+                            user.getWorld().addBlockBreakParticles(user.getBlockPos().up(), Blocks.GLASS.getDefaultState());
+                            user.playSound(SoundEvents.BLOCK_GLASS_BREAK, 0.43f, 1.3f);
+                            user.playSound(SoundEvents.BLOCK_FIRE_EXTINGUISH, 0.6f, 1.7f);
+                            stack.decrement(1);
+                            return TypedActionResult.consume(stack);
+                        }
+                    }else{
+                        if(!user.getWorld().isClient()){
+                            ((ServerWorld)user.getWorld()).spawnParticles(ParticleTypes.FLASH, user.getX(), user.getY(), user.getZ(), 1,0,0, 0, 0.1);
+                            user.sendMessage(Text.literal(LightWithin.PREFIX_MSG).formatted(Formatting.AQUA).append(Text.translatable("item.lightwithin.bottled_light.exploded").formatted(Formatting.YELLOW)));
+                        }
+                        stack.decrement(stack.getCount());
+                        ItemEntity item = new ItemEntity(world, user.getX(), user.getY()+1, user.getZ(), stack);
+                        world.createExplosion(item, user.getX(), user.getY()+1, user.getZ(), 1.5f, false, World.ExplosionSourceType.MOB);
+                        return TypedActionResult.consume(stack);
+                    }
+                }
+
             }
             return TypedActionResult.pass(stack);
         }
@@ -148,7 +193,19 @@ public class BottledLightItem extends Item {
     public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type) {
         tooltip.add(Text.translatable("item.lightwithin.bottled_light.tooltip"));
         if(Screen.hasShiftDown()) {
-            if(stack.getComponents().contains(LightItemComponents.BOTTLED_LIGHT_PLAYER_UUID) && stack.getComponents().get(LightItemComponents.BOTTLED_LIGHT_PLAYER_UUID).equals(UUID.fromString("00000000-0000-0000-0000-000000000000"))){
+            if(stack.contains(DataComponentTypes.CUSTOM_DATA) && stack.get(DataComponentTypes.CUSTOM_DATA) != null){
+                NbtComponent nbtComponent = stack.get(DataComponentTypes.CUSTOM_DATA);
+                if(nbtComponent.contains(BrewRecipes.TYPE_INGREDIENT_KEY) && nbtComponent.copyNbt().getUuid(BrewRecipes.PLAYER_NBT_KEY).equals(UUID.fromString("00000000-0000-0000-0000-000000000000"))){
+                    tooltip.add(Text.translatable("item.lightwithin.bottled_light.tooltip.warning").formatted(Formatting.AQUA).formatted(Formatting.ITALIC));
+                    if(nbtComponent.contains(BrewRecipes.TYPE_INGREDIENT_KEY)){
+                        tooltip.add(Text.translatable("item.lightwithin.bottled_light.tooltip.type").formatted(Formatting.GREEN).append(Text.literal(Objects.requireNonNull(nbtComponent.copyNbt().getString(BrewRecipes.TYPE_INGREDIENT_KEY))).formatted(Formatting.LIGHT_PURPLE)));
+                    }
+                    if(nbtComponent.contains(BrewRecipes.TARGET_INGREDIENT_KEY)){
+                        tooltip.add(Text.translatable("item.lightwithin.bottled_light.tooltip.target").formatted(Formatting.GREEN).append(Text.literal(Objects.requireNonNull(nbtComponent.copyNbt().getString(BrewRecipes.TARGET_INGREDIENT_KEY))).formatted(Formatting.LIGHT_PURPLE)));
+                    }
+                }
+            }
+            else if(stack.getComponents().contains(LightItemComponents.BOTTLED_LIGHT_PLAYER_UUID) && stack.getComponents().get(LightItemComponents.BOTTLED_LIGHT_PLAYER_UUID).equals(UUID.fromString("00000000-0000-0000-0000-000000000000"))){
                 tooltip.add(Text.translatable("item.lightwithin.bottled_light.tooltip.warning").formatted(Formatting.AQUA).formatted(Formatting.ITALIC));
                 if(stack.contains(LightItemComponents.BOTTLED_LIGHT_TYPE_INGREDIENT)){
                     tooltip.add(Text.translatable("item.lightwithin.bottled_light.tooltip.type").formatted(Formatting.GREEN).append(Text.literal(Objects.requireNonNull(stack.get(LightItemComponents.BOTTLED_LIGHT_TYPE_INGREDIENT))).formatted(Formatting.LIGHT_PURPLE)));
@@ -156,7 +213,8 @@ public class BottledLightItem extends Item {
                 if(stack.contains(LightItemComponents.BOTTLED_LIGHT_TARGET_INGREDIENT)){
                     tooltip.add(Text.translatable("item.lightwithin.bottled_light.tooltip.target").formatted(Formatting.GREEN).append(Text.literal(Objects.requireNonNull(stack.get(LightItemComponents.BOTTLED_LIGHT_TARGET_INGREDIENT))).formatted(Formatting.LIGHT_PURPLE)));
                 }
-            }else{
+            }
+            else{
                 tooltip.add(Text.literal("§bPlayer UUID: §a"+getCreatedBy(stack).toString()));
             }
 
@@ -169,6 +227,15 @@ public class BottledLightItem extends Item {
 
     public static UUID getCreatedBy(ItemStack bottle_item){
         if(!bottle_item.getComponents().contains(LightItemComponents.BOTTLED_LIGHT_PLAYER_UUID)){
+            //Support for backwards compatibility
+            if(bottle_item.contains(DataComponentTypes.CUSTOM_DATA)){
+                NbtComponent nbtComponent = bottle_item.get(DataComponentTypes.CUSTOM_DATA);
+                if(nbtComponent != null && nbtComponent.contains(BrewRecipes.PLAYER_NBT_KEY)){
+                    return nbtComponent.copyNbt().getUuid(BrewRecipes.PLAYER_NBT_KEY);
+                }
+            }
+
+            //If there is no nbt component
             return UUID.fromString("00000000-0000-0000-0000-000000000000");
         }
         return bottle_item.get(LightItemComponents.BOTTLED_LIGHT_PLAYER_UUID);
