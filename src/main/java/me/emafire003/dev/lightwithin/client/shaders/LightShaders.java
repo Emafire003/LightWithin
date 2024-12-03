d7e0f43f6fa22e1f7915937aaf8238b69fe6780b
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2024-12-03 13:00:25 +0100

    Added warning message to disable the intoxicated shader

--- src/main/java/me/emafire003/dev/lightwithin/client/shaders/LightShaders.java
+++ src/main/java/me/emafire003/dev/lightwithin/client/shaders/LightShaders.java
@@ -6,6 +6,8 @@ import ladysnake.satin.api.managed.ShaderEffectManager;
 import me.emafire003.dev.lightwithin.config.ClientConfig;
 import me.emafire003.dev.lightwithin.status_effects.LightEffects;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 import java.util.Objects;
@@ -21,6 +23,8 @@ public class LightShaders {
     public static final ManagedShaderEffect WOBBLE_SHADER = ShaderEffectManager.getInstance()
             .manage(new Identifier("shaders/post/wobble.json"));
 
+    private static boolean intoxicated_warning_sent = false;
+
     public static void registerShaders(){
         ShaderEffectRenderCallback.EVENT.register(tickDelta -> {
             if(ClientConfig.FORESTAURA_INTOXICATION_SHADER){
@@ -31,19 +35,26 @@ public class LightShaders {
     }
 
     public static void registerIntoxicationShader(float tickDelta){
-        if(MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().player.hasStatusEffect(LightEffects.INTOXICATION)){
-
-            /*if(ClientConfig.INTOXICATION_SHADER_WARNING){
+        if(MinecraftClient.getInstance().player != null
+                && MinecraftClient.getInstance().player.hasStatusEffect(LightEffects.INTOXICATION)
+        ){
+            if(ClientConfig.INTOXICATION_SHADER_WARNING && !intoxicated_warning_sent){
+                intoxicated_warning_sent = true;
                 MinecraftClient.getInstance().player.sendMessage(Text.translatable("lightwithin.warning.general").formatted(Formatting.RED, Formatting.BOLD)
-                        .append(Text.literal(" ").append(Text.translatable("lightwithin.warning.intoxication_shader.1").formatted(Formatting.WHITE))));
+                        .append(Text.literal(" §r").append(Text.translatable("lightwithin.warning.intoxication_shader.1").formatted(Formatting.WHITE))));
 
-            }*/
+            }
             if(Objects.requireNonNull(MinecraftClient.getInstance().player.getStatusEffect(LightEffects.INTOXICATION)).getAmplifier() > 5){
                 WOBBLE_SHADER.render(tickDelta);
             }else{
                 DECONVERGE_SHADER.render(tickDelta);
                 PHOSPHOR_DRUNK_SHADER.render(tickDelta);
             }
+        }
+        if(MinecraftClient.getInstance().player != null && intoxicated_warning_sent
+                && !MinecraftClient.getInstance().player.hasStatusEffect(LightEffects.INTOXICATION)
+        ){
+            intoxicated_warning_sent = false;
         }
     }
 }
