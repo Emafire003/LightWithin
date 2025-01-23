1f86507df25d3a2e2f8491eb0abe9261e620e4e0
emacarioli <73383820+emacarioli@users.noreply.github.com>
2025-01-23 13:08:10 +0100

    Now the glowing entities from the forest aura effect get updated

--- src/main/java/me/emafire003/dev/lightwithin/status_effects/ForestAuraEffect.java
+++ src/main/java/me/emafire003/dev/lightwithin/status_effects/ForestAuraEffect.java
@@ -5,6 +5,7 @@ import me.emafire003.dev.lightwithin.compat.coloredglowlib.CGLCompat;
 import me.emafire003.dev.lightwithin.networking.GlowEntitiesPacketS2C;
 import me.emafire003.dev.lightwithin.util.CheckUtils;
 import me.emafire003.dev.lightwithin.util.ForestAuraRelation;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.LivingEntity;
@@ -26,9 +27,8 @@ import static me.emafire003.dev.lightwithin.LightWithin.BOX_EXPANSION_AMOUNT;
 
 /**
  * Players will be able to pass through natural blocks, the one in the tag list,
- * will not suffocate in these
+ * will not suffocate in these. For more info see {@link me.emafire003.dev.lightwithin.mixin.forest_aura_related.ForestBlocksMixin}
  * and will also see the entities that are nearby them when they get the effect for the first time*/
-//TODO maybe update the entites list every 5 seconds or something?
 public class ForestAuraEffect extends StatusEffect {
 
     public ForestAuraEffect() {
@@ -37,39 +37,45 @@ public class ForestAuraEffect extends StatusEffect {
 
     List<LivingEntity> visibleEntities = new ArrayList<>();
 
-    // This method is called when it applies the status effect. We implement custom functionality here.
+    /**How much time between updating the list of entities which the player will be able to see*/
+    private static final int ticksBetweenUpdates = 20*3; //Aka it now updates the entities once every 3 secons
+    private int tickCounter = 0;
+
+    //for some reason this does not work, so work-arounds!
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-
+        super.applyUpdateEffect(entity, amplifier);
     }
 
-    //TODO maybe the number of entites you can see is dependent on the power level? Like at level one you see only like 3 entites
-    // , and then +1 for each level, then at level 7+ you can distinguish between allies and enemies?(if CLG is installed)
-    @Override
-    public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
-        super.onApplied(entity, attributes, amplifier);
-        if(!(entity instanceof PlayerEntity)){
+    AtomicInteger visibleEntityCounter = new AtomicInteger(0);
+
+    /**Updates the entites that are glowing if the player still hasn't filled up all of the "slots",
+     * aka the amount of entities they can see which amplifier+3
+     * This is a workaround since applyUpdatedEffect is not working
+     *
+     * @param entity The target entity with the effect
+     * @param amplifier The amplifier of the effect
+     */
+    public void updateVisibleEntities(LivingEntity entity, int amplifier){
+        if(visibleEntityCounter.get() == amplifier+3){
+            //When the counter is already past max entities return early
             return;
         }
-
-        //TODO write in wiki:
-        // You will be able to see a number of entites equal to 3+the power level.
-        // At level 7 you will see if they are enemies and
-        // at level 9 you will see if they are allies, enemies or neutral.
-        // if cgl is installed
-        AtomicInteger counter = new AtomicInteger(0);
-        visibleEntities = entity.getWorld().getEntitiesByClass(LivingEntity.class, new Box(entity.getBlockPos()).expand(BOX_EXPANSION_AMOUNT*1.5), (entity1 -> {
-            if(counter.get() < amplifier+3){
-                counter.getAndIncrement();
-                return !entity.equals(entity1);
+        List<LivingEntity> newVisibleEntities = entity.getWorld().getEntitiesByClass(LivingEntity.class, new Box(entity.getBlockPos()).expand(BOX_EXPANSION_AMOUNT * 1.5), (entity1 -> {
+            if (visibleEntityCounter.get() < amplifier + 3) {
+                if(!entity.equals(entity1) && !visibleEntities.contains(entity1)){
+                    visibleEntityCounter.getAndIncrement();
+                    return true;
+                }
             }
             return false;
         }));
+        visibleEntities.addAll(newVisibleEntities);
 
-        //0 mean neutral, 1 means enemy, 2 ally
+        //0 means neutral, 1 means enemy, 2 ally
         List<Pair<UUID, ForestAuraRelation>> uuids_related = new ArrayList<>();
 
-        visibleEntities.forEach(entity1 -> {
+        newVisibleEntities.forEach(entity1 -> {
 
             if(FabricLoader.getInstance().isModLoaded("coloredglowlib")){
                 if(amplifier >= 6){
@@ -92,7 +98,7 @@ public class ForestAuraEffect extends StatusEffect {
                     }
                     if(!colored){
                         uuids_related.add(new Pair<>(entity1.getUuid(), ForestAuraRelation.NEUTRAL));
-                       // CGLCompat.getLib().setExclusiveColorFor(entity1, ForestAuraLight.COLOR, (PlayerEntity) entity);
+                        // CGLCompat.getLib().setExclusiveColorFor(entity1, ForestAuraLight.COLOR, (PlayerEntity) entity);
                     }
                 }
                 else{
@@ -104,13 +110,44 @@ public class ForestAuraEffect extends StatusEffect {
                 uuids_related.add(new Pair<>(entity1.getUuid(), ForestAuraRelation.NEUTRAL));
                 //uuids_related.add(new Pair<>(entity1.getUuid(), 0));
             }
-
-
         });
+
         if(!entity.getWorld().isClient()){
-            //TODO remove
             GlowEntitiesPacketS2C glowingPacket = new GlowEntitiesPacketS2C(uuids_related, false);
             ServerPlayNetworking.send((ServerPlayerEntity) entity, GlowEntitiesPacketS2C.ID, glowingPacket);
+        }
+    }
+
+    @Override
+    public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        super.onApplied(entity, attributes, amplifier);
+
+        if(!(entity instanceof PlayerEntity)){
+            return;
+        }
+
+        //TODO write in wiki:
+        // You will be able to see a number of entites equal to 3+the power level.
+        // At level 7 you will see if they are enemies and
+        // at level 9 you will see if they are allies, enemies or neutral.
+        // if cgl is installed
+        //allows the counter to work again
+        tickCounter = 0;
+        //Makes the initial entities in the area glow
+        updateVisibleEntities(entity, amplifier);
+
+        //This is a required work-around since applyUpdateEffect doesn't work for some weird reason
+        if(!entity.getWorld().isClient()){
+            ServerTickEvents.END_SERVER_TICK.register(server -> {
+                if(tickCounter == -1){
+                    return;
+                }
+                if(tickCounter >= ticksBetweenUpdates && entity.hasStatusEffect(LightEffects.FOREST_AURA)){
+                    updateVisibleEntities(entity, amplifier);
+                    tickCounter = 0;
+                }
+                tickCounter++;
+            });
         }
 
     }
@@ -128,6 +165,11 @@ public class ForestAuraEffect extends StatusEffect {
             GlowEntitiesPacketS2C glowingPacket = new GlowEntitiesPacketS2C(null, true);
             ServerPlayNetworking.send((ServerPlayerEntity) entity, GlowEntitiesPacketS2C.ID, glowingPacket);
         }
+        //clears the stuff
+        visibleEntities.clear();
+        visibleEntityCounter.set(0);
+        //Stops the counter until it's necessary again
+        tickCounter = -1;
     }
 
 }
