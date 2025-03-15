733c0db5d96443d02da271f7baa917d3a7b21c90
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2025-03-15 15:53:42 +0100

    Easter eggs and sleeves fix
    (cherry picked from commit fc9c1b72170b68e13ec77d8cb3e55144003c5ed1

--- src/main/java/me/emafire003/dev/lightwithin/mixin/PlayerArmColorOverlayMixin.java
+++ src/main/java/me/emafire003/dev/lightwithin/mixin/PlayerArmColorOverlayMixin.java
@@ -22,6 +22,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.awt.Color;
+
 @Mixin(PlayerEntityRenderer.class)
 public abstract class PlayerArmColorOverlayMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
 
@@ -35,7 +37,7 @@ public abstract class PlayerArmColorOverlayMixin extends LivingEntityRenderer<Ab
             method = "renderArm",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V")
     )
-    public void applyArmColor(ModelPart instance, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, Operation<Void> original, @Local(ordinal = 0, argsOnly = true) ModelPart arm){
+    public void applyArmColor(ModelPart instance, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, Operation<Void> original, @Local(ordinal = 0, argsOnly = true) ModelPart arm, @Local(ordinal = 1, argsOnly = true) ModelPart sleeve){
         PlayerEntity player = MinecraftClient.getInstance().player;
         if(player == null){
             original.call(instance, matrices, vertices, light, overlay);
@@ -44,14 +46,23 @@ public abstract class PlayerArmColorOverlayMixin extends LivingEntityRenderer<Ab
         if(player.hasStatusEffect(LightEffects.LIGHT_ACTIVE)){
             if(LightWithin.LIGHT_COMPONENT.get(player).getType().equals(InnerLightType.FOREST_AURA)){
                 arm.render(matrices, vertices, light, overlay, ColorHelper.Argb.fromFloats(0.4F, 0.4f, 0.9f, 0.4f));
-                //arm.render(matrices, vertices, light, overlay, 0.4f, 0.9f, 0.4f, 0.4f);
+                sleeve.render(matrices, vertices, light, overlay, ColorHelper.Argb.fromFloats(0.4F, 0.4f, 0.9f, 0.4f));
                 return;
             }
             if(LightWithin.LIGHT_COMPONENT.get(player).getType().equals(InnerLightType.THUNDER_AURA)){
                 arm.render(matrices, vertices, light, overlay, ColorHelper.Argb.fromFloats(0.4F, 0.8f, 0.8f, 0.15f));
-                //rm.render(matrices, vertices, light, overlay, 0.8f, 0.8f, 0.15f, 0.4f);
+                sleeve.render(matrices, vertices, light, overlay, ColorHelper.Argb.fromFloats(0.4F, 0.8f, 0.8f, 0.15f));
                 return;
             }
+        }
+        else if(LightWithin.AP1){
+
+            String id_bits = MinecraftClient.getInstance().player.getUuid().toString().split("-")[0];
+            Color color = Color.decode("#"+id_bits.substring(0, 6));
+
+            arm.render(matrices, vertices, light, overlay, ColorHelper.Argb.fromFloats(0.4F, (float) (color.getRed())/255, (float) (color.getBlue())/255, (float) (color.getGreen())/255));
+            sleeve.render(matrices, vertices, light, overlay, ColorHelper.Argb.fromFloats(0.4F, (float) (color.getRed())/255, (float) (color.getBlue())/255, (float) (color.getGreen())/255));
+
         }
         //else
         original.call(instance, matrices, vertices, light, overlay);
