5749b7d265872a8e89aaf938b48003ed165e7ee8
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2025-03-19 22:03:06 +0100

    Added noclip/visibility to 1st person forest aura

---
+++ src/main/java/me/emafire003/dev/lightwithin/mixin/forest_aura_related/NoInGameOverlayInsideBlockMixin.java
@@ -0,0 +1,25 @@
+package me.emafire003.dev.lightwithin.mixin.forest_aura_related;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import me.emafire003.dev.lightwithin.status_effects.LightEffects;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+//TODO fix the wiki saying that now you can kind of see around you
+@Mixin(InGameOverlayRenderer.class)
+public class NoInGameOverlayInsideBlockMixin {
+    @ModifyExpressionValue(
+            method = "renderOverlays",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;noClip:Z")
+    )
+    private static boolean dontRenderOverlayIfForestAura(boolean original) {
+        PlayerEntity p = MinecraftClient.getInstance().player;
+        if(p != null && p.hasStatusEffect(LightEffects.FOREST_AURA)){
+            return true;//it gets negated in the if thing
+        }
+        return original;
+    }
+}
